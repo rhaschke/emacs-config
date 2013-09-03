980b2ef98fe17cb8f5478198cbec03c5e3575fd6
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2013-09-03 14:16:52 +0200

    emacsconfig: set default browser

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(browse-url-generic-program "chromium-browser" t)
  '(column-number-mode t)
  '(delete-selection-mode nil nil (delsel))
  '(ecb-layout-name "left3")
