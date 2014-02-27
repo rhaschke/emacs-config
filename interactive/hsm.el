2a663659955dc722acbe17b84caee4d34cf103a7
Robert Haschke <rhaschke@techfak>
2014-02-27 15:11:36 +0100

    first version for HSM semantic parser

--- interactive/hsm.el
+++ interactive/hsm.el
@@ -6,10 +6,6 @@
 
 (add-to-list 'auto-mode-alist '("\\.hsm$" . hsm-mode))
 
-;; regular expression to generate outline in hsm mode
-(setq-mode-local hsm-mode
-					  nxml-section-element-name-regexp ".*STATE\\|REGION")
-
 ;; hsm error display
 (defun display-hsm-errors (message)
   (with-current-buffer (get-buffer-create "*hsm errors*")
@@ -51,3 +47,96 @@
 					buffer-file-name))
 )
 (add-hook 'hsm-mode-hook 'rhaschke/hsm-mode-hook)
+
+
+(defun semantic-hsm-parse-region (start end &rest ignore)
+  "Parse the current hsm buffer for semantic tags."
+  (let ((tags (semantic-nxml-parse-region start end)))
+	 (destructuring-bind (states events) (semantic-hsm-transform-tags tags)
+		;; summarize several states within HSM node
+		(when (> (length states) 1)
+		  (setq start (semantic-tag-start (first states))
+				  end (semantic-tag-end (car (last states)))
+				  states (semantic-tag "HSM" 'state :states states))
+		  (semantic-tag-set-bounds states start end))
+		(nconc states (nreverse events)))))
+		
+(defun semantic-hsm-tag-components (tag)
+  "Return list of components of TAG"
+  (let ((states (semantic-tag-get-attribute tag :children))
+		  (events (semantic-tag-get-attribute tag :attributes)))
+	 (append states events)))
+
+(defun semantic-hsm-tag-components-with-overlays (tag)
+  "Return list of components of TAG"
+  (semantic-hsm-tag-components tag))
+
+(defvar state-regexp ".*STATE\\|REGION")
+
+(defun semantic-hsm-transform-tags (tags)
+  "Remap nxml tags into (states events) list
+states is in correct order, events is reverse"
+  (let (states events)
+	 (dolist (tag tags)
+		(let ((class (semantic-tag-class tag))
+				(name  (semantic-tag-name tag))
+				(atts  (semantic-tag-get-attribute tag :attributes))
+				(children (semantic-tag-get-attribute tag :children)))
+		  (when (eq class 'element)
+			 (cond 
+			  ;; handle states
+			  ((string-match-p state-regexp name)
+				(let* ((att (semantic-find-first-tag-by-name "name" atts))
+						 (name (semantic-tag-get-attribute att :value))
+						 (stateTag (semantic-tag name 'state)))
+				  (apply 'semantic-tag-set-bounds stateTag (semantic-tag-bounds tag))
+				  (destructuring-bind (s e) (semantic-hsm-transform-tags children)
+					 (semantic-tag-put-attribute stateTag :states s)
+					 (setq events (append e events)))
+				  (push stateTag states)))
+			  ;; other elements
+			  (t
+				(destructuring-bind (s e) (semantic-hsm-transform-tags children)
+				  (assert (not states))
+				  (setq events (append e events)
+						  states s)))))))
+			 
+	 (list (nreverse states) events)))
+					 
+
+(semantic-install-function-overrides
+ '(
+	(parse-region . semantic-hsm-parse-region)
+	(tag-components . semantic-hsm-tag-components)
+	(tag-components-with-overlays . semantic-hsm-tag-components-with-overlays)
+	(format-tag-abbreviate . semantic-nxml-format-tag-abbreviate)
+	(format-tag-summarize . semantic-nxml-format-tag-summarize)
+	)
+ t 'hsm-mode)
+
+(require 'ecb)
+;; define how the new tag classes should be displayed in ecb-methods-buffer
+(let ((defaults (car (cdar (get 'ecb-show-tags 'standard-value))))
+		(hsm-settings '(hsm-mode (element flattened nil)
+										 (attribute hidden nil)
+										 (include collapsed nil))))
+  (nconc defaults (list hsm-settings)))
+
+(let ((defaults (car (cdar (get 'ecb-tag-display-function 'standard-value))))
+		(hsm-settings '(hsm-mode . ecb-format-tag-summarize)))
+  (nconc defaults (list hsm-settings)))
+
+;; define faces for states and events
+(nconc semantic-format-face-alist '((state . font-lock-function-name-face)
+												(event . font-lock-variable-name-face)))
+
+;;;###autoload
+(defun semantic-default-hsm-setup ()
+  "Setup hook function for hsm and semantic."
+  (setq
+	semantic-parser-name  "hsm"
+   semantic-symbol->name-assoc-list '((state . "states")
+												  (event . "events"))))
+
+;;;###autoload
+(add-hook 'hsm-mode-hook 'semantic-default-hsm-setup)
