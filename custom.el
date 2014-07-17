53d8837f97c31cf23c195745674ef72b34b845a1
Robert Haschke <rhaschke@techfak>
2014-07-17 14:39:59 +0200

    order changed by emacs

--- custom.el
+++ custom.el
@@ -1,8 +1,8 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(browse-url-generic-program "chromium-browser" t)
  '(column-number-mode t)
  '(delete-selection-mode nil nil (delsel))
@@ -13,17 +13,14 @@
  '(ecb-tip-of-the-day nil)
  '(grep-find-command "find . -type f -not -path \"*/.svn*\" -print0 | \"xargs\" -0 -e grep -nH -e ")
  '(mouse-drag-copy-region t)
- '(safe-local-variable-values (quote 
-										 ((ispell-dictionary . "british") 
-										  (ispell-dictionary . "de_DE")
-										  (ispell-dictionary . "american"))))
- '(show-paren-mode t)
- '(scroll-bar-mode 'right)
  '(nxml-auto-insert-xml-declaration-flag nil)
- '(nxml-slash-auto-complete-flag t))
+ '(nxml-slash-auto-complete-flag t)
+ '(safe-local-variable-values (quote ((c-file-offsets (substatement-open . 0)) (ispell-dictionary . "british") (ispell-dictionary . "de_DE") (ispell-dictionary . "american"))))
+ '(scroll-bar-mode (quote right))
+ '(show-paren-mode t))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
