5789158828224473ed8e61273d5b5cd7dceffe62
Robert Haschke <rhaschke@techfak>
2014-08-27 15:56:29 +0200

    added git svn example

--- interactive/magit.el
+++ interactive/magit.el
@@ -1,5 +1,7 @@
 (require 'magit-autoloads)
 
 ;; http://magit.github.io/magit.html#Activating-extensions
+;; e.g. to activate magit-svn, exec the following git cmd in your repository:
+;; git config --add magit.extension svn
 (add-hook 'magit-mode-hook 'magit-load-config-extensions)
 
