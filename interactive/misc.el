34c5982bcc064172051862bd4ef5b78886cf27ed
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2014-09-23 02:34:56 -0500

    improve normalize-whitespace functions

--- interactive/misc.el
+++ interactive/misc.el
@@ -25,7 +25,26 @@ If FRAME is nil, apply to current frame."
 
 (defun normalize-whitespace (start end)
   "remove trailing whitespace, remove empty lines at end of document"
-  (interactive "r")
+  (interactive (progn
+                 (barf-if-buffer-read-only)
+                 (if (use-region-p)
+                     (list (region-beginning) (region-end))
+                   (list nil nil))))
   (save-excursion
-	(indent-region start end))
-  (delete-trailing-whitespace))
+	(let ((have-region (and start end))
+		  (end (or end (point-max)))
+		  (start (or start (point-min))))
+	  (indent-region start end)
+	  (if have-region (delete-trailing-whitespace start end)
+		(delete-trailing-whitespace)))))
+
+(defun dired-do-normalize-whitespace ()
+  "apply normalize-whitespace function to all marked files in dired"
+  (interactive)
+  (dired-map-over-marks 
+   (progn 
+	 (dired-find-file)
+	 (normalize-whitespace nil nil)
+	 (save-buffer)
+	 (kill-buffer)
+	 ) nil))
