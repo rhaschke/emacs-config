773f44bbf55dd66b2641ab77ac6e8bd71b06be39
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2015-04-22 09:22:51 +0200

    improved usage of pretty-print-xml-region()

--- interactive/nxml.el
+++ interactive/nxml.el
@@ -2,7 +2,7 @@
 (add-to-list 'auto-mode-alist '("\\.me$"  . nxml-mode))
 (add-to-list 'auto-mode-alist '("\\.rnc$" . rnc-mode))
 
-(defun bf-pretty-print-xml-region (begin end)
+(defun pretty-print-xml-region (begin end)
   "Pretty format XML markup in region. You need to have nxml-mode
    http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
    this.  The function inserts linebreaks to separate tags that
@@ -10,11 +10,13 @@
    markup by using nxml's indentation rules."
   (interactive "r")
   (save-excursion
+    (let ((begin (or begin (point-min)))
+          (end (or end (point-max))))
       (nxml-mode)
       (goto-char begin)
       (while (search-forward-regexp "\>[ \\t]*\<" nil t) 
         (backward-char) (insert "\n"))
-      (indent-region begin end)))
+      (indent-region begin end))))
 
 (custom-set-variables
  '(nxml-auto-insert-xml-declaration-flag nil)
